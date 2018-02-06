4a20829a198b99b31cf1ab5831264dbf9310f743
Denis Borisevichus <borideni@gmail.com>
2018-02-06 10:48:47 +0300

    parse branch name for last words v1

--- format-commit-msg.sh
+++ format-commit-msg.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #
-# Automatically adds branch name for commit message
+# Automatically adds last part of branch name for commit message
 # to the beginning of the line. Result is:
 # <branch_name>: <commit_msg>#
 
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
+#BRANCH_NAME=$(git symbolic-ref --short HEAD)
+BRANCH_NAME=$(git symbolic-ref --short HEAD | rev | cut -d'_' -f1 | rev)
 COMMIT_MSG=$(head -n 1 $1)
 
 if [ -n "$BRANCH_NAME" ] &&
